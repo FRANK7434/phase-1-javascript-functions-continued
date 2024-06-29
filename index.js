bc162cb2eb9740498f1fda659d524ad14a5f8af5
FRANK7434 <sonfransiscojames015@gmail.com>
2024-06-29 22:32:37 +0300

    added functions continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("bathe my dog"))
+console.log(saturdayFun()); 
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(sir="*"){
+    return function(wish="hard worker"){
+        return`You are ${sir}${wish}${sir}!`;
+    }
+}
